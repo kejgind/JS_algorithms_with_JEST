1167f56c9dad890dae483532a05b79f16f2fc84e
Krzysztof G <kejgind@gmail.com>
2018-05-26 10:06:19 +0200

    27. Convert HTML Entities test/solution

---
+++ 27_convert_htm_entities/convertHTML.js
@@ -0,0 +1,9 @@
+const convertHTML = str =>
+  str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+
+module.exports = convertHTML;
